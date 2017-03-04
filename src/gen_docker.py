c5b366f2a1774cac1f1d1c076dfd3b4e3c4f47d6
kaczmarj <jakubk@mit.edu>
2017-03-04 10:33:38 -0500

    enh: make dockerfile class

--- src/gen_docker.py
+++ src/gen_docker.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, print_function
 import argparse
 from distutils.version import LooseVersion
+import os.path as op
 
 LATEST_PYTHON = "3.6.0"
 
@@ -37,6 +38,140 @@ def get_docker_args():
     return vars(args)
 
 
+
+class Dockerfile(object):
+    """Dockerfile object.
+
+    Parameters
+    ----------
+    software : dict
+        Software specifications for the Dockerfile. Keys include 'base',
+        'py_version', and 'py_pkgs' (so far)
+    """
+    def __init__(self, software):
+        self.software = software
+        self._cmds = []  # List of Dockerfile commands.
+
+    def add_base(self):
+        """Return Dockerfile command to use the baseimage `base`.
+
+        Parameters
+        ----------
+        base : str
+            The base image and its version (e.g., Ubuntu:16.04). Does the base
+            image have to be hosted on Dockerhub?
+        """
+        cmd = "FROM {}".format(self.software['base'])
+        self._cmds.append(cmd)
+
+    def add_miniconda(self, miniconda_version='latest'):
+        """Return Dockerfile command to install Miniconda.
+
+        Parameters
+        ----------
+        py_version : str
+            Python version (e.g., '3.5.2')
+        miniconda_version : str
+            Miniconda version. Defaults to 'latest'.
+        """
+        py_version = self.software['py_version']
+        if py_version is None: return
+
+        comments = ["# Install miniconda.",
+                    "# Define environment variables.",
+                    "# Install Python {}.".format(py_version)]
+
+        base_url = "https://repo.continuum.io/miniconda/"
+        install_file = ("Miniconda{}-{}-Linux-x86_64.sh"
+                        "".format(py_version[0], miniconda_version))
+        install_url = base_url + install_file
+
+        # curl and bzip2 have to be installed for this to work.
+        install_cmd = ("curl -sSLO {url}\n"
+                       "/bin/bash {file} -b -p /usr/local/miniconda\n"
+                       "rm {file}".format(url=install_url, file=install_file))
+        # Add Docker RUN command and indent lines.
+        install_cmd = indent("RUN", install_cmd, line_suffix=" && \\")
+
+        # Clean this up.
+        env_cmd = ("PATH=/usr/local/miniconda/bin:$PATH\n"
+                   "PYTHONPATH=/usr/local/miniconda/lib/python{}/site-packages\n"
+                   "PYTHONNOUSERSITE=1\n"
+                   "LANG=C.UTF-8\n"
+                   "LC_ALL=C.UTF-8"
+                   "".format(py_version[:3]))
+        # Add Docker command ENV and indent lines.
+        env_cmd = indent("ENV", env_cmd, line_suffix=" \\")
+
+        # Install the requested Python version. Is there a way to install this
+        # version of Python when installing Miniconda? In the latest
+        # installation shell script, it looks like Miniconda defaults to
+        # Python 3.6.
+        conda_python_cmd = ("RUN conda install python={}".format(py_version))
+
+        cmd = "\n".join((comments[0], install_cmd, comments[1], env_cmd,
+                        comments[2], conda_python_cmd))
+        # cmd = "{}\n{}\n{}\n{}".format(comments[0], install_cmd, comments[1],
+        #                              env_cmd, comments[2], conda_python_cmd)
+        self._cmds.append(cmd)
+
+    def add_python_pkgs(self):
+        """Return Dockerfile command to install Python packages.
+
+        Parameters
+        ----------
+        py_pkgs : list
+            List of tuple and/or str. Tuples must have two items, the first
+            being the name of the package and the second being the version.
+            String must be name of package.
+        """
+        if self.software['py_pkgs'] is None: return
+
+        if not self.software['py_version']:
+            raise Exception("Python version must be specified in order to add "
+                            "Python packages")
+
+        comment = "# Install Python packages."
+        conda_install_list = []
+        for item in self.software['py_pkgs']:
+            if isinstance(item, tuple):  # e.g., ('numpy', 1.10)
+                # e.g., numpy==1.10
+                install_str = "{}=={}".format(item[0], item[1])
+                conda_install_list.append(install_str)
+            elif isinstance(item, str):  # e.g., 'numpy'
+                conda_install_list.append(item)
+
+        add_channel_cmd = "conda config --add channels conda-forge"
+        install_cmd = "conda install -y " + ' '.join(conda_install_list)
+
+        # cmd = "{}\n{}".format(add_channel_cmd, install_cmd)
+        cmd = "\n".join((add_channel_cmd, install_cmd))
+        cmd = indent("RUN", cmd, line_suffix=" && \\")
+
+        # cmd = "{}\n{}".format(comment, cmd)
+        cmd = "\n".join((comment, cmd))
+        self._cmds.append(cmd)
+
+    def add_command(self, cmd):
+        """
+        cmd : str
+            Command to be added.
+        """
+        self._cmds.append(cmd)
+
+    def create(self):
+        """Return Dockerfile as string."""
+        return '\n\n'.join(self._cmds)
+
+    def save(self):
+        """Save Dockerfile."""
+        fname = "Dockerfile"
+        obj = self.create()
+        with open(fname, 'w') as fp:
+            fp.write(obj)
+
+
+
 def indent(docker_cmd, text, line_suffix=''):
     """docstring"""
     amount = len(docker_cmd) + 1
@@ -57,105 +192,16 @@ def indent(docker_cmd, text, line_suffix=''):
     return out
 
 
-def install_base(base):
-    return "FROM {}".format(base)
-
-
-def install_miniconda(py_version, miniconda_version='latest'):
-    """"""
-    comment = "# Install miniconda."
-    base_url = "https://repo.continuum.io/miniconda/"
-    install_file = "Miniconda{}-{}-Linux-x86_64.sh".format(py_version[0],
-                                                           miniconda_version)
-    install_url = base_url + install_file
-
-    install_cmd = ("curl -sSLO {install_url}\n"
-                   "/bin/bash {install_file} -b -p /usr/local/miniconda\n"
-                   "rm {install_file}"
-                   "".format(install_url=install_url,
-                             install_file=install_file))
-    # Add Docker RUN command and indent lines.
-    install_cmd = indent("RUN", install_cmd, line_suffix=" && \\")
-
-    env_cmd = ("PATH=/usr/local/miniconda/bin:$PATH\n"
-               # "PYTHONPATH=/usr/local/miniconda/lib/python{}/site-packages\n"
-               "PYTHONNOUSERSITE=1\n"
-               "LANG=C.UTF-8\n"
-               "LC_ALL=C.UTF-8"
-               "".format(py_version[:3]))
-    # Add Docker command ENV and indent lines.
-    env_cmd = indent("ENV", env_cmd, line_suffix=" \\")
-
-    conda_env_cmd = ("conda install python={py_version}"
-                     "".format(py_version=py_version))
-    conda_env_cmd = indent("RUN", conda_env_cmd, line_suffix=" && \\")
-
-    return "{}\n{}\n{}\n{}".format(comment, install_cmd, env_cmd, conda_env_cmd)
-
-
-# Only run this if at least one python package is requested.
-def install_conda_pkgs(py_pkgs):
-    """Install conda packages. Should list of tuples be param?"""
-    comment = "# Install conda packages."
-
-    conda_install_list = []
-    for item in py_pkgs:
-        if isinstance(item, tuple):
-            # e.g., numpy==1.10
-            install_str = "{}=={}".format(item[0], item[1])
-            conda_install_list.append(install_str)
-        elif isinstance(item, str):
-            # e.g., numpy
-            conda_install_list.append(item)
-
-    add_channel_cmd = "conda config --add channels conda-forge"
-    install_cmd = "conda install -y " + ' '.join(conda_install_list)
-
-    cmd = "{}\n{}".format(add_channel_cmd, install_cmd)
-    cmd = indent("RUN", cmd, line_suffix=" && \\")
-
-    return "{}\n{}".format(comment, cmd)
-
-
-def create_dockerfile_str(kwargs, reqs):
-    docker_out = []
-
-    if kwargs['base']:
-        cmd = install_base(kwargs['base'])
-        docker_out.append(cmd)
-
-    docker_out.append(reqs)
-
-    if kwargs['py_version']:
-        cmd = install_miniconda(kwargs['py_version'])
-        docker_out.append(cmd)
-
-    if kwargs['py_pkgs']:
-        cmd = install_conda_pkgs(kwargs['py_pkgs'])
-        docker_out.append(cmd)
-
-    return '\n\n'.join(docker_out)
-
-
-def save_dockerfile(fname, info):
-    """"""
-    with open(fname, 'w') as fp:
-        fp.write(info)
-
-
 def main():
     software = get_docker_args()
+    reqs_cmd = ("RUN apt-get update && apt-get install -y bzip2 curl")
 
-    reqs = ("apt-get update && apt-get install -y\n"
-            "curl\n"
-            "bzip2")
-    reqs = indent("RUN", reqs, line_suffix=' \\')
-
-    dockerfile = create_dockerfile_str(software, reqs)
-    fname = "Dockerfile"
-    save_dockerfile(fname, dockerfile)
-
-    print(dockerfile)
+    df = Dockerfile(software)
+    df.add_base()
+    df.add_command(reqs_cmd)
+    df.add_miniconda()
+    df.add_python_pkgs()
+    df.save()
 
 
 if __name__ == "__main__":
