f262a9277d8975025cdae1b61fe9685b1ea30a20
jakubk <jakubk@mit.edu>
2017-03-08 12:03:20 -0500

    fix: get correct yaml formatting on save

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,11 @@ import logging
 import os.path as op
 import sys
 
+try:
+    import urllib.request as urllib  # Python 3
+except ImportError:
+    import urllib2 as urllib  # Python 2
+
 import ruamel_yaml as yaml
 
 
@@ -40,7 +45,7 @@ def set_log_level(level):
 def load_yaml(filepath):
     """Load YAML file as Python dictionary."""
     with open(filepath, 'r') as stream:
-        return yaml.safe_load(stream)
+        return yaml.load(stream, Loader=yaml.RoundTripLoader)
 
 
 def save_yaml(obj, filepath):
@@ -49,5 +54,19 @@ def save_yaml(obj, filepath):
     # if op.exists(filepath):
     #     raise Exception("File already exists: {}".format(filepath))
     with open(filepath, 'w') as stream:
-        yaml.safe_dump(obj, stream, default_flow_style=False,
-                       block_seq_indent=2)
+        yaml.dump(obj, stream, Dumper=yaml.RoundTripDumper, block_seq_indent=2)
+
+
+def check_url(url):
+    """Return true if `url` is reachable. Otherwise, log warning and return
+    false.
+    http://stackoverflow.com/a/16778473/5666087
+    """
+    try:
+        # Is `with` necessary? Do we have to close the url?
+        # Read http://stackoverflow.com/a/1522709/5666087
+        with urllib.urlopen(url):
+            return True
+    except (urllib.HTTPError, urllib.URLError) as e:
+        logger.warning("{} error on URL {}".format(url, e))
+        return False
