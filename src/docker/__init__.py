8716561eb0ff5045dc60e40132099707c15963b1
jakubk <jakubk@mit.edu>
2017-03-09 19:08:42 -0500

    moved files to docker directory

---
+++ src/docker/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+from .docker_image import DockerImage
+from .dockerfile import Dockerfile
+from .fsl import FSL
+from .miniconda import Miniconda
+from .utils import SpecsParser
