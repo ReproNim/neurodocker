22666fd5d045faa8b07f12e113b9558d16893db5
jakubk <jakubk@mit.edu>
2017-03-30 08:54:02 -0400

    enh: build with gcc-4.9 and g++-4.9

--- src/docker/ants.py
+++ src/docker/ants.py
@@ -2,33 +2,49 @@
 
 Project repository: https://github.com/stnava/ANTs/
 
-Binaries for versions 2.0.0 and newer are available in the GitHub repository,
-(and older versions are available on SourceForge), but ANTs recommends building
-from source.
+ANTs recommends building from source. Versions 2.0.0 and newer are available
+on GitHub, and older versions are available on SourceForge.
 
-Build instructions:
+Build instructions (takes approximately 40 minutes):
 https://github.com/stnava/ANTs/wiki/Compiling-ANTs-on-Linux-and-Mac-OS
 
-Dockerfile commands to build ANTs from source:
+When building ANTs versions 2.0.1 and 2.1.0, ITK build broke because of gcc
+(version 5.x?). Getting an older version of gcc (4.9) fixed that issue.
 
-    FROM ubuntu:16.04
-    RUN apt-get update && apt-get install -y cmake g++ git zlib1g-dev
-    RUN git clone git://github.com/stnava/ANTs.git && \
-        mkdir antsbin && \
-        cd antsbin && \
-        cmake ../ANTs && \
-        make -j 2
-    ENV ANTSPATH=/antsbin/bin/
-    ENV PATH=$ANTSPATH:$PATH
+Dockerfile commands to build ANTs from source on Ubuntu 16.04:
 
+    #-------------
+    # Install ANTs
+    #-------------
+    RUN apt-get update -qq && \
+        add-apt-repository ppa:ubuntu-toolchain-r/test && apt-get update && \
+        apt-get install -yq --no-install-recommends \
+        ca-certificates \
+        cmake \
+        gcc-4.9 \
+        g++-4.9 \
+        git \
+        make \
+        zlib1g-dev && \
+        apt-get clean && \
+        rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
-Dockerfile commands to download ANTs binaries:
+    RUN ln -s /usr/bin/gcc-4.9 /usr/bin/cc && \
+        ln -s /usr/bin/gcc-4.9 /usr/bin/gcc && \
+        ln -s /usr/bin/g++-4.9 /usr/bin/g++ && \
+        ln -s /usr/bin/g++-4.9 /usr/bin/c++
 
-    FROM ubuntu:16.04
-    RUN apt-get update && apt-get install -y bzip2 curl
-    RUN curl -LO <URL> && \
-        tar zxvf <TARBALL>
-    ENV PATH=/path/to/ants/bin:$PATH
+    WORKDIR /tmp
+    RUN git clone git://github.com/stnava/ANTs.git &&  \
+        cd ANTs &&  \
+        # ANTs 2.1.0
+        git checkout 78931aa6c4943af25e0ee0644ac611d27127a01e &&  \
+        mkdir build && cd build && cmake .. && make -j 2 &&  \
+        mkdir -p /opt/ants &&  \
+        cp bin/* /opt/ants && cp ../Scripts/* /opt/ants &&  \
+        cd /tmp && rm -rf ANTs
+    ENV ANTSPATH=/opt/ants/ \
+        PATH=/opt/ants:$PATH
 """
 from __future__ import absolute_import, division, print_function
 
@@ -42,7 +58,8 @@ class ANTs(object):
     Inspired by the Dockerfile at https://hub.docker.com/r/nipype/workshops/
     `docker pull nipype/workshops:latest-nofsspm`
     """
-    VERSION_HASHES = {"2.1.0": "78931aa6c4943af25e0ee0644ac611d27127a01e",
+    VERSION_HASHES = {"latest": None,
+                      "2.1.0": "78931aa6c4943af25e0ee0644ac611d27127a01e",
                       "2.1.0rc3": "465cc8cdf0f8cc958edd2d298e05cc2d7f8a48d8",
                       "2.1.0rc2": "17160f72f5e1c9d6759dd32b7f2dc0b36ded338b",
                       "2.1.0rc1": "1593a7777d0e6c8be0b9462012328bde421510b9",
@@ -52,11 +69,12 @@ class ANTs(object):
                       "2.0.0": "7ae1107c102f7c6bcffa4df0355b90c323fcde92",
                       "HoneyPot": "7ae1107c102f7c6bcffa4df0355b90c323fcde92",}
 
-    BUILD_DEPENDENCIES = {"centos": ['cmake', 'g++', 'git', 'zlib-devel'],
-                          "debian": ['cmake', 'build-essential', 'git',
-                                     'zlib1g-dev'],
-                          "ubuntu": ['cmake', 'build-essential', 'git',
-                                     'zlib1g-dev'],}
+    BUILD_DEPENDENCIES = {"centos": ['ca-certificates', 'cmake', 'g++', 'git',
+                                     'make', 'zlib-devel'],
+                          "debian": ['ca-certificates', 'cmake', 'gcc-4.9',
+                                     'g++-4.9', 'git', 'make', 'zlib1g-dev'],
+                          "ubuntu": ['ca-certificates', 'cmake', 'gcc-4.9',
+                                     'g++-4.9', 'git', 'make', 'zlib1g-dev'],}
 
     def __init__(self, version, os):
         self.version = version
@@ -74,7 +92,9 @@ class ANTs(object):
 
     def _create_cmd(self):
         """"""
-        comment = "#-------------\n# Install ANTs\n#-------------"
+        comment = ("#-------------------\n"
+                   "# Install ANTs {}\n"
+                   "#-------------------".format(self.version))
         chunks = [comment, self.get_dependencies(),
                   self.build_from_source_github()]
         return "\n".join(chunks)
@@ -85,13 +105,29 @@ class ANTs(object):
         deps = sorted(deps)
         deps = "\n".join(deps)
         if self.os in ['debian', 'ubuntu']:
-            cmd = ("apt-get update -qq && apt-get install -yq --no-install-recommends\n"
+            cmd = ("apt-get update -qq &&\n"
+                   "add-apt-repository ppa:ubuntu-toolchain-r/test &&\n"
+                   "apt-get update &&\n"
+                   "apt-get install -yq --no-install-recommends\n"
                    "{} &&\n"
                    "apt-get clean &&\n"
                    "rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*".format(deps))
+            symlinks = ("ln -s /usr/bin/gcc-4.9 /usr/bin/cc &&\n"
+                        "ln -s /usr/bin/gcc-4.9 /usr/bin/gcc &&\n"
+                        "ln -s /usr/bin/g++-4.9 /usr/bin/g++ &&\n"
+                        "ln -s /usr/bin/g++-4.9 /usr/bin/c++")
+            cmd = indent("RUN", cmd)
+            symlinks = indent("RUN", symlinks)
+            cmd = "\n".join((cmd, symlinks))
+
         elif self.os in ['centos']:
-            pass
-        return indent("RUN", cmd)
+            cmd = ("yum -y install\n"
+                   "{} &&\n"
+                   "yum clean packages &&\n"
+                   "rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*".format(deps))
+            cmd = indent("RUN", cmd)
+
+        return cmd
 
     def build_from_source_github(self):
         """Return Dockerfile instructions to build ANTs from source hosted on
@@ -100,13 +136,18 @@ class ANTs(object):
         version_hash = ANTs.VERSION_HASHES[self.version]
         workdir_cmd = "WORKDIR /tmp"
 
+        if self.version == "latest":
+            checkout = ""
+        else:
+            checkout = "git checkout {} && \n".format(version_hash)
+
         cmd = ("git clone https://github.com/stnava/ANTs.git && \n"
                "cd ANTs && \n"
-               "git checkout {} && \n"
+               "{}"
                "mkdir build && cd build && cmake .. && make && \n"
                "mkdir -p /opt/ants && \n"
                "cp bin/* /opt/ants && cp ../Scripts/* /opt/ants && \n"
-               "cd /tmp && rm -rf ANTs".format(version_hash))
+               "cd /tmp && rm -rf ANTs".format(checkout))
         cmd = indent("RUN", cmd)
 
         env_cmd = ("ANTSPATH=/opt/ants/\n"
