1c1baa18fd3d9fbf038aea39a8bf1682f1788100
mathiasg <mathiasg@mit.edu>
2017-04-12 18:12:50 -0400

    fix+tst: adding motion correction tests, making ds003 spm compatible

--- tests/nipype/spm_realign.py
+++ tests/nipype/spm_realign.py
@@ -1,19 +1,17 @@
-# vi: set ft=python sts=4 ts=4 sw=4 et:
 from __future__ import absolute_import
 import os
 import argparse
 
 from bids.grabbids import BIDSLayout
 from nipype import Node
-from nipype.interfaces.afni import Skullstrip()
+from nipype.interfaces.spm import Realign
 
 from .utils import base_version, env_to_json
 
 # setup environment tracker
 ENV = base_version()
 # make this more dynamic
-OUTDIR = '/data/afni'
-
+OUTDIR = '/data/spm'
 
 def create_workflow():
     parser = argparse.ArgumentParser()
@@ -28,21 +26,21 @@ def create_workflow():
     # grab data from bids structure
     layout = BIDSLayout(args.data)
     subj = layout.get_subjects()[0]
-    t1 = [f.filename for f in layout.get(subject=subj, type='T1w',
-                                         extensions=['nii.gz'])][0]
+    func = [f.filename for f in layout.get(subject=subj, type='bold',
+                                           extensions=['nii.gz'])][0]
 
     outfile = os.path.join(OUTDIR, 'test_{}_{}'.format(subj, ENV['os']))
 
-    # run afni skullstrip
-    skullstrip = SkullStrip()
-    skullstrip.inputs.in_file = t1
-    skullstrip.inputs.outputtype = 'NIFTI_GZ'
+    # run interface
+    realign = Realign()
+    realign.inputs.in_files = func
     # FIX: this has to be unique for each environment
-    skullstrip.inputs.out_file = outfile + '.nii.gz'
-    res = skullstrip.run()
+    realign.inputs.out_prefix = outfile
+    res = mcflirt.run()
 
     # write out json to keep track of information
     ENV.update({'inputs': res.inputs})
+    ENV.update({'nipype_version': nipype.__version__})
     #ENV.update({'outputs': res.outputs})
     # write out to json
     env_to_json(ENV, outname=outfile + '.json')
