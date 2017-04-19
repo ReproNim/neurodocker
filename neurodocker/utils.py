1e1ba7eca22082100147c9e21ab9d751b06966cb
jakubk <jakubk@mit.edu>
2017-04-19 09:55:17 -0400

    move funcs to here from neurodocker.docker.utils

--- neurodocker/utils.py
+++ neurodocker/utils.py
@@ -1,15 +1,30 @@
 """Package utility functions."""
+# Author: Jakub Kaczmarzyk <jakubk@mit.edu>
 from __future__ import absolute_import, division, print_function
 import json
 import logging
-import os.path as op
 import sys
 import warnings
 
 import requests
 
+
+# Templates for installing packages and cleaning up with apt and yum.
+manage_pkgs = {'apt': {'install': ('apt-get update -qq && apt-get install -yq '
+                                   '--no-install-recommends {pkgs}'),
+                       'remove': 'apt-get purge -y --auto-remove {pkgs}',
+                       'clean': ('apt-get clean '
+                                '&& rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*'),},
+               'yum': {'install': 'yum install -y -q {pkgs}',
+                       # Trying to uninstall ca-certificates breaks things.
+                       'remove': 'yum remove -y $(echo "{pkgs}" | sed "s/ca-certificates//g")',
+                       'clean': ('yum clean packages '
+                                 '&& rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*'),},
+                }
+
 # Create logger.
-logger = logging.getLogger('nipype_regtests')
+# TODO: move this to main program.
+logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 ch = logging.StreamHandler(sys.stdout)  # Print to console.
 ch.setLevel(logging.DEBUG)
@@ -78,3 +93,74 @@ def check_url(url, timeout=5, **kwargs):
         warnings.warn("URL ({}) returned status code {}."
                       "".format(url, request.status_code))
         return False
+
+
+def indent(instruction, cmd, line_suffix=' \\'):
+    """Add Docker instruction and indent command.
+
+    Parameters
+    ----------
+    instruction : str
+        Docker instruction for `cmd` (e.g., "RUN").
+    cmd : str
+        The command (lines separated by newline character).
+    line_suffix : str
+        The suffix to append to each line except the last one.
+
+    Returns
+    -------
+    dockerfile_chunk : str
+        Instruction compatible with Dockerfile sytax.
+    """
+    instruction = instruction.upper()
+    amount = len(instruction) + 1
+    indent = ' ' * amount
+    split_cmd = cmd.splitlines()
+
+    if len(split_cmd) == 1:
+        return "{} {}".format(instruction, cmd)
+
+    dockerfile_chunk = ''
+    for i, line in enumerate(split_cmd):
+        if i == 0:  # First line.
+            dockerfile_chunk += "{} {}{}".format(instruction, line, line_suffix)
+        # Change the following to use str.join() method.
+        elif i == len(split_cmd) - 1:  # Last line.
+            dockerfile_chunk += "\n{}{}".format(indent, line)
+        else:
+            dockerfile_chunk += "\n{}{}{}".format(indent, line, line_suffix)
+    return dockerfile_chunk
+
+
+def add_neurodebian(os_codename, full=True, check_urls=True):
+    """Return instructions to add NeuroDebian to Dockerfile.
+
+    Parameters
+    ----------
+    os_codename : str
+        Operating system codename (e.g., 'zesty', 'jessie'). Used in the
+        NeuroDebian url: http://neuro.debian.net/lists/OS_CODENAME.us-nh.full.
+    full : bool
+        If true, use the full NeuroDebian sources. If false, use the libre
+        sources.
+    check_urls : bool
+        If true, throw warning if URLs relevant to the installation cannot be
+        reached.
+    """
+    suffix = "full" if full else "libre"
+    neurodebian_url = ("http://neuro.debian.net/lists/{}.us-nh.{}"
+                       "".format(os_codename, suffix))
+    if check_urls:
+        check_url(neurodebian_url)
+
+    cmd = ("deps='dirmngr wget'\n"
+           "&& {install}\n"
+           "&& wget -O- {url} >> "
+           "/etc/apt/sources.list.d/neurodebian.sources.list\n"
+           "&& apt-key adv --recv-keys --keyserver "
+           "hkp://pool.sks-keyservers.net:80 0xA5D32F012649A5A9\n"
+           "&& apt-get update\n"
+           "&& {remove}\n"
+           "&& {clean}".format(url=neurodebian_url, **manage_pkgs['apt']))
+    cmd = cmd.format(pkgs="$deps")
+    return indent("RUN", cmd)
