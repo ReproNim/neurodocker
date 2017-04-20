2d8137d14a5abb883b9f0cc5dc9b490ef8882f05
jakubk <jakubk@mit.edu>
2017-04-19 20:37:58 -0400

    build docker image with binaries

--- neurodocker/interfaces/tests/test_ants.py
+++ neurodocker/interfaces/tests/test_ants.py
@@ -1,52 +1,32 @@
-"""Tests for src.docker.ants.ANTs"""
+"""Tests for neurodocker.interfaces.ANTs"""
+# Author: Jakub Kaczmarzyk <jakubk@mit.edu>
 from __future__ import absolute_import, division, print_function
+from io import BytesIO
 
 import pytest
 
+from neurodocker.docker_api import Dockerfile, DockerImage, DockerContainer
+from neurodocker.parser import SpecsParser
 from neurodocker.interfaces import ANTs
 
 
 class TestANTs(object):
     """Tests for ANTs class."""
-    def install_binaries_apt(self):
-        # --- Version 2.1.0 ---
-        # apt
-        url = "https://www.dropbox.com/s/x7eyk125bhwiisu/ants-2.1.0_centos-5.tar.gz?dl=1"
-        test = ("#-------------------\n"
-                "# Install ANTs 2.1.0\n"
-                "#-------------------\n"
-                "WORKDIR /opt\n"
-                "RUN deps='ca-certificates wget' \\\n"
-                "    && apt-get update -qq && apt-get install -yq --no-install-recommends $deps \\\n"
-                "    && wget -qO ants.tar.gz {url} \\\n"
-                "    && tar xzf ants.tar.gz \\\n"
-                "    && rm -f ants.tar.gz \\\n"
-                "    && apt-get purge -y --auto-remove $deps\n"
-                "ENV ANTSPATH=/opt/ants\n"
-                "ENV PATH=$ANTSPATH:$PATH".format(url=url))
-        ants = ANTs(version='2.1.0', pkg_manager='apt', use_binaries=True)
-        assert ants.cmd == test, "command to install ANTs binaries not correct (apt)"
+    def install_binaries_centos7(self):
+        """Install ANTs 2.1.0 binaries on Ubuntu."""
+        specs = {'base': 'centos:7',
+                 'software': {
+                     'ants': {'version': '2.1.0', 'use_binaries': True}}}
+        parser = SpecsParser(specs=specs)
+        cmd = Dockerfile(specs=parser.specs, pkg_manager='yum').cmd
+        fileobj = BytesIO(cmd.encode('utf-8'))
 
-        # yum
-        url = "https://www.dropbox.com/s/x7eyk125bhwiisu/ants-2.1.0_centos-5.tar.gz?dl=1"
-        test = ("#-------------------\n"
-                "# Install ANTs 2.1.0\n"
-                "#-------------------\n"
-                "WORKDIR /opt\n"
-                "RUN deps='ca-certificates wget' \\\n"
-                "    && yum install -y -q $deps \\\n"
-                "    && wget -qO ants.tar.gz {url} \\\n"
-                "    && tar xzf ants.tar.gz \\\n"
-                "    && rm -f ants.tar.gz \\\n"
-                "    && yum autoremove -y -q\n"
-                "ENV ANTSPATH=/opt/ants\n"
-                "ENV PATH=$ANTSPATH:$PATH".format(url=url))
-        ants = ANTs(version='2.1.0', pkg_manager='yum', use_binaries=True)
-        assert ants.cmd == test, "command to install ANTs binaries not correct (yum)"
-
-        # --- next version ---
-        # apt
-        # yum
+        image = DockerImage(fileobj=fileobj).build_raw()
+        container = DockerContainer(image)
+        container.start()
+        output = container.exec_run('Atropos')
+        assert "error" not in output, "error running bet"
+        container.cleanup(remove=True, force=True)
 
     def test_build_from_source_github(self):
         # --- latest version (master branch) ---
