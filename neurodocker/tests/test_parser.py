3a6d92e7a861174171d3c3e0bbf1ea5a0bae192c
jakubk <jakubk@mit.edu>
2017-05-01 12:02:26 -0400

    complete testing for neurodocker.parser

--- neurodocker/tests/test_parser.py
+++ neurodocker/tests/test_parser.py
@@ -1,7 +1,6 @@
 """Tests for neurodocker.parser"""
 # Author: Jakub Kaczmarzyk <jakubk@mit.edu>
 from __future__ import absolute_import
-import os
 
 import pytest
 
@@ -9,23 +8,54 @@ from neurodocker.parser import SpecsParser
 
 
 class TestSpecsParser(object):
+    @pytest.fixture(autouse=True)
+    def setup(self, tmpdir):
+        self.tmpdir = tmpdir
 
-    def test_init(self):
-        specs = {'base': 'ubuntu:16.04',
-                 'pkg_manager': 'apt',
-                 'miniconda': {'conda_install': ['traits'],
-                               'pip_install': ['https://github.com/nipy/nipype/archive/master.tar.gz'],
-                               'python_version': '3.5.1'},
-                 'ants': {'use_binaries': True, 'version': '2.1.0'},
-                 'fsl': {'use_binaries': True, 'version': '5.0.8'}}
-        parser = SpecsParser(specs=specs)
+    def test_str(self):
+        specs = {'base': 'ubuntu:17.04',
+                 'pkg_manager': 'yum'}
+        assert str(SpecsParser(specs)) == str(specs)
 
-        correct = {'base': 'ubuntu:16.04',
-                   'pkg_manager': 'apt',
-                   'miniconda': {'conda_install': 'traits',
-                                 'pip_install': 'https://github.com/nipy/nipype/archive/master.tar.gz',
-                                 'python_version': '3.5.1'},
-                    'ants': {'use_binaries': True, 'version': '2.1.0'},
-                    'fsl': {'use_binaries': True, 'version': '5.0.8'}}
+    def test_use_dict_or_json(self):
+        import os.path as op
+        from neurodocker.utils import save_json
+        specs = {'base': 'ubuntu:17.04',
+                 'pkg_manager': 'yum'}
+        filepath = op.join(self.tmpdir.strpath, 'specs.json')
+        save_json(specs, filepath)
+        assert SpecsParser(specs).specs == SpecsParser(filepath).specs
 
-        assert parser.specs == correct, "error parsing conda and/or pip packages"
+    def test__validate_keys(self):
+        # Missing base image.
+        specs = {'pkg_manager': 'apt'}
+        with pytest.raises(KeyError):
+            SpecsParser(specs)
+
+        # Missing package manager.
+        specs = {'base': 'ubuntu:17.04'}
+        with pytest.raises(KeyError):
+            SpecsParser(specs)
+
+        # Invalid top-level key.
+        specs = {'base': 'ubuntu:17.04',
+                 'pkg_manager': 'yum',
+                 'fake_software': 'fake_val',}
+        with pytest.raises(KeyError) as e:
+            SpecsParser(specs)
+        assert 'fake_software' in str(e.value)
+
+    def test__parse_conda_pip(self):
+        specs_l = {'base': 'ubuntu:17.04',
+                   'pkg_manager': 'yum',
+                   'miniconda': {'python_version': '3.6.1',
+                                 'conda_install': ['nipype', 'numpy'],
+                                 'pip_install': ['pandas', 'pylsl']}
+                   }
+        specs_s = {'base': 'ubuntu:17.04',
+                   'pkg_manager': 'yum',
+                   'miniconda': {'python_version': '3.6.1',
+                                 'conda_install': 'nipype numpy',
+                                 'pip_install': 'pandas pylsl'}
+                   }
+        assert SpecsParser(specs_l).specs == SpecsParser(specs_s).specs
