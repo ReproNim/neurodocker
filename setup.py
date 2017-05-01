ff5cd79506634d02f7d1c6e56df01540b6a30de8
jakubk <jakubk@mit.edu>
2017-05-01 16:36:52 -0400

    add find_packages()

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(name='neurodocker',
       version='dev',
       url='https://github.com/kaczmarj/neurodocker',
       author='Jakub Kaczmarzyk',
       author_email='jakubk@mit.edu',
-      packages=['neurodocker'],
+      packages=find_packages(),
       install_requires = ['docker>=2.2.0', 'requests>=2.0.0'],
       )
