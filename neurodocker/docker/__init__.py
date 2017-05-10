fc5eef8cb914b6465b75c01c22adc63930474390
jakubk <jakubk@mit.edu>
2017-05-10 12:34:06 -0400

    add helpful error message if docker is not installed

--- neurodocker/docker/__init__.py
+++ neurodocker/docker/__init__.py
@@ -1,3 +1,9 @@
 from __future__ import absolute_import
 
+try:
+    import docker
+except:
+    raise ImportError("The docker Python package is required to use features "
+                      "that interact with the Docker Engine.")
+
 from neurodocker.docker.docker import client, DockerContainer, DockerImage
