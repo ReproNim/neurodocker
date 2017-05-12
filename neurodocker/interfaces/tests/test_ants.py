3de294f3492f7a3f796c162113c8b4298347e900
jakubk <jakubk@mit.edu>
2017-05-12 17:24:28 -0400

    expand on tests

--- neurodocker/interfaces/tests/test_ants.py
+++ neurodocker/interfaces/tests/test_ants.py
@@ -1,6 +1,9 @@
 """Tests for neurodocker.interfaces.ANTs"""
 # Author: Jakub Kaczmarzyk <jakubk@mit.edu>
+
 from __future__ import absolute_import, division, print_function
+
+import pytest
 
 from neurodocker.interfaces import ANTs
 from neurodocker.interfaces.tests import utils
@@ -13,12 +16,26 @@ class TestANTs(object):
         """Install ANTs 2.1.0 binaries on CentOS 7."""
         specs = {'base': 'centos:7',
                  'pkg_manager': 'yum',
-                 'check_urls': False,
+                 'check_urls': True,
                  'ants': {'version': '2.1.0', 'use_binaries': True}}
         container = utils.get_container_from_specs(specs)
         output = container.exec_run('Atropos')
         assert "error" not in output, "error running Atropos"
         utils.test_cleanup(container)
+
+    def test_invalid_binaries(self):
+        with pytest.raises(ValueError):
+            ANTs(version='fakeversion', pkg_manager='apt', check_urls=False)
+
+    def test_install_with_custom_url(self):
+        url = 'http://fakesite'
+        ants = ANTs(version='2.2.0', pkg_manager='apt', use_binaries=url,
+                    check_urls=False)
+        assert url in ants.cmd
+
+        with pytest.raises(ValueError):
+            ants = ANTs(version='2.2.0', pkg_manager='apt',
+                        use_binaries='fakesite', check_urls=False)
 
     def test_build_from_source_github(self):
         # TODO: expand on tests for building ANTs from source. It probably
@@ -26,7 +43,11 @@ class TestANTs(object):
         # time limit. It takes about 45 minutes to compile ANTs.
 
         ants = ANTs(version='latest', pkg_manager='apt', use_binaries=False)
+        assert "git checkout" not in ants.cmd
+
+        ants = ANTs(version='2.2.0', pkg_manager='yum', use_binaries=False)
         assert ants.cmd
 
-        ants = ANTs(version='latest', pkg_manager='yum', use_binaries=False)
-        assert ants.cmd
+        ants = ANTs(version='arbitrary', pkg_manager='apt', use_binaries=False,
+                    git_hash='12345')
+        assert 'git checkout 12345' in ants.cmd
