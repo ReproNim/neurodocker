0d62d0a13672c5f6e78981afaa203f5c2dda9086
jakubk <jakubk@mit.edu>
2017-06-26 08:52:57 -0400

    import reprozip

--- neurodocker/interfaces/__init__.py
+++ neurodocker/interfaces/__init__.py
@@ -4,4 +4,5 @@ from neurodocker.interfaces.ants import ANTs
 from neurodocker.interfaces.fsl import FSL
 from neurodocker.interfaces.miniconda import Miniconda
 from neurodocker.interfaces.mrtrix import MRtrix3
+from neurodocker.interfaces.reprozip import Reprozip
 from neurodocker.interfaces.spm import SPM
