d68f5b34e70d9bca9cfbfce960dec9816c769457
jakubk <jakubk@mit.edu>
2017-07-25 16:42:26 -0400

    update utils to new specs dictionary

--- neurodocker/interfaces/tests/utils.py
+++ neurodocker/interfaces/tests/utils.py
@@ -1,13 +1,12 @@
 """Utility functions for `neurodocker.interfaces.tests`."""
 from __future__ import absolute_import
 
-from neurodocker import Dockerfile, SpecsParser
+from neurodocker import Dockerfile
 from neurodocker.docker import client, DockerContainer, DockerImage
 
 def get_container_from_specs(specs, **kwargs):
     """Return started container. `kwargs` are for `container.start()`."""
-    parser = SpecsParser(specs)
-    df = Dockerfile(parser.specs)
+    df = Dockerfile(specs)
     image = DockerImage(df).build(log_console=True)
     return DockerContainer(image).start(**kwargs)
 
