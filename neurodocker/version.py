09a9755174c5d778abbbf4ad64fce1388134eb98
jakubk <jakubk@mit.edu>
2017-08-16 17:51:07 -0400

    up to dev version + use git describe is possible

--- neurodocker/version.py
+++ neurodocker/version.py
@@ -3,7 +3,7 @@
 Copied from https://github.com/nipy/nipype/blob/master/nipype/info.py.
 """
 
-__version__ = '0.2.0'
+__version__ = '0.3.0-dev0'
 
 
 def get_gitversion():
@@ -20,7 +20,7 @@ def get_gitversion():
     here = os.path.dirname(os.path.realpath(__file__))
 
     try:
-        cmd = 'git describe --tags'.split()
+        cmd = 'git describe'.split()
         stdout, stderr = subprocess.Popen(cmd, cwd=here,
                                           stdout=subprocess.PIPE,
                                           stderr=subprocess.PIPE).communicate()
@@ -31,9 +31,8 @@ def get_gitversion():
     return ver
 
 
-if __version__.endswith('-dev'):
-    gitversion = get_gitversion()
-    if gitversion:
-        __version__ = gitversion
-        if gitversion.startswith('v'):
-            __version__ = __version__[1:]
+gitversion = get_gitversion()
+if gitversion:
+    __version__ = gitversion
+    if gitversion.startswith('v'):
+        __version__ = __version__[1:]
