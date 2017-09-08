261b817daf6b423aa660a9333ae0acb00b0060bb
jakubk <jakubk@mit.edu>
2017-09-08 18:24:16 -0400

    enh: rename dockerfile.py to generate.py

--- neurodocker/__init__.py
+++ neurodocker/__init__.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import, unicode_literals
 from neurodocker.version import __version__
 
 from neurodocker.docker import DockerContainer, DockerImage
-from neurodocker.dockerfile import Dockerfile
+from neurodocker.generate import Dockerfile
 from neurodocker.utils import set_log_level
 
 set_log_level('info')
