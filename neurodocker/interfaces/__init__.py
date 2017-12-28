dd8a69bba8e98bb3dfa8148eb26aad973746b298
Jakub Kaczmarzyk <jakubk@mit.edu>
2017-12-28 16:57:40 -0500

    add init file

--- neurodocker/interfaces/__init__.py
+++ neurodocker/interfaces/__init__.py
@@ -1,3 +1,5 @@
+"""Module-level interface imports"""
+
 from neurodocker.interfaces.interfaces import (
     AFNI, AFNI, ANTs, Convert3D, Dcm2niix, FSL, FreeSurfer, MatlabMCR, MINC,
     Miniconda, MRtrix3, NeuroDebian, PETPVC, SPM12
