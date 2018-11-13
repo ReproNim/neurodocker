4f9db9145398bcbb4f12f7742ff0f5d2b612fbbb
Jakub Kaczmarzyk <jakubk@mit.edu>
2018-11-13 16:51:47 -0500

    add: nd_freeze interface

--- neurodocker/neurodocker.py
+++ neurodocker/neurodocker.py
@@ -27,6 +27,7 @@ logger = logging.getLogger(__name__)
 # https://stackoverflow.com/a/9028031/5666087
 class OrderedArgs(Action):
     """Object to preserve order in which command-line arguments are given."""
+
     def __call__(self, parser, namespace, values, option_string=None):
         if 'ordered_args' not in namespace:
             setattr(namespace, 'ordered_args', [])
@@ -48,102 +49,140 @@ def _add_generate_common_arguments(parser):
     p.add_argument(
         "-b", "--base", help="Base Docker image. E.g., debian:stretch")
     p.add_argument(
-        "-p", "--pkg-manager", choices={'apt', 'yum'},
+        "-p",
+        "--pkg-manager",
+        choices={'apt', 'yum'},
         help="Linux package manager.")
     p.add_argument(
-        '--add-to-entrypoint', action=OrderedArgs,
+        '--add-to-entrypoint',
+        action=OrderedArgs,
         help=("Add a command to the file /neurodocker/startup.sh, which is the"
               " container's default entrypoint."))
     p.add_argument(
-        '--copy', action=OrderedArgs, nargs="+",
+        '--copy',
+        action=OrderedArgs,
+        nargs="+",
         help="Copy files into container. Use format <src>... <dest>")
     p.add_argument(
-        '--install', action=OrderedArgs, nargs="+",
+        '--install',
+        action=OrderedArgs,
+        nargs="+",
         help=("Install system packages with apt-get or yum, depending on the"
               " package manager specified."))
     p.add_argument(
-        '--entrypoint', action=OrderedArgs,
+        '--entrypoint',
+        action=OrderedArgs,
         help="Set the container's entrypoint (Docker) / append to runscript"
-             " (Singularity)")
+        " (Singularity)")
     p.add_argument(
-        '-e', '--env', action=OrderedArgs, nargs="+", type=_list_of_kv,
+        '-e',
+        '--env',
+        action=OrderedArgs,
+        nargs="+",
+        type=_list_of_kv,
         help="Set environment variable(s). Use the format KEY=VALUE")
     p.add_argument(
-        '-r', '--run', action=OrderedArgs,
+        '-r',
+        '--run',
+        action=OrderedArgs,
         help="Run a command when building container")
     p.add_argument(
         '--run-bash', action=OrderedArgs, help="Run a command in bash")
     p.add_argument(
-        '-u', '--user', action=OrderedArgs,
+        '-u',
+        '--user',
+        action=OrderedArgs,
         help="Switch current user (creates user if necessary)")
     p.add_argument(
         '-w', '--workdir', action=OrderedArgs, help="Set working directory")
 
     # To generate from file.
     p.add_argument(
-        'file', nargs='?',
+        'file',
+        nargs='?',
         help="Generate file from JSON. Overrides other `generate` arguments")
     p.add_argument(
         '--json', action="store_true", help="Print Neurodocker JSON spec")
 
     # Other arguments (no order).
     p.add_argument(
-        '-o', '--output', dest="output",
+        '-o',
+        '--output',
+        dest="output",
         help="If specified, save Dockerfile to file with this name.")
     p.add_argument(
-        '--no-print', dest='no_print', action="store_true",
+        '--no-print',
+        dest='no_print',
+        action="store_true",
         help="Do not print the generated file")
 
     _ndeb_servers = ", ".join(
-        _installation_implementations['neurodebian']._servers.keys()
-    )
+        _installation_implementations['neurodebian']._servers.keys())
 
     # Software package options.
     pkgs_help = {
-        "all": "Install software packages. Each argument takes a list of"
-               " key=value pairs. Where applicable, the default installation"
-               " behavior is to install by downloading and uncompressing"
-               " binaries. Some programs can be built from source.",
-        "afni": "Install AFNI. Valid keys are version (required), method,"
-                " install_path, install_r, install_r_pkgs, install_python2,"
-                " and install_python3. Only the latest version and version"
-                " 17.2.02 are supported at this time.",
-        "ants": "Install ANTs. Valid keys are version (required), method"
-                " install_path, cmake_opts, and make_opts. Version can be a "
-                " git commit hash if building from source.",
-        "convert3d": "Install Convert3D. Valid keys are version (required),"
-                     " method, and install_path.",
-        "dcm2niix": "Install dcm2niix. Valid keys are version, method,"
-                    " install_path, cmake_opts, and make_opts",
-        "freesurfer": "Install FreeSurfer. Valid keys are version (required),"
-                      " method, install_path, and exclude_paths. A FreeSurfer"
-                      " license is required to run the software and is not"
-                      " provided by Neurodocker.",
-        "fsl": "Install FSL. Valid keys are version (required), method,"
-               " install_path, and exclude_paths.",
-        "matlabmcr": "Install Matlab Compiler Runtime. Valid keys are version,"
-                     " method, and install_path",
-        "miniconda": "Install Miniconda. Valid keys are install_path,"
-                     " env_name, conda_install, pip_install, conda_opts,"
-                     " pip_opts, activate (default false), and version"
-                     " (defaults to latest). The options conda_install and"
-                     " pip_install accept strings of packages: conda_install="
-                     '"python=3.6 numpy traits".',
-        "mrtrix3": "Install MRtrix3. Valid keys are version (required),"
-                   " method, and install_path",
-        "neurodebian": "Add NeuroDebian repository. Valid keys are "
-                       "os_codename (e.g., zesty), server (e.g., usa-nh), and"
-                       " full (if true, use non-free packages). Valid download"
-                       " servers are {}.".format(_ndeb_servers),
-        "spm12": "Install SPM12 and its dependency, Matlab Compiler Runtime."
-                 " Valid keys are version and install_path.",
-        "minc": "Install MINC. Valid keys is version (required), method, and"
-                " install_path. Only version 1.9.15 is supported at this"
-                " time.",
-        "petpvc": "Install PETPVC. Valid keys are version (required), method,"
-                  " and install_path.",
-        "vnc": "Install a VNC server. Valid keys are passwd (required),"
-               " start_at_runtime, and geometry."
+        "all":
+        "Install software packages. Each argument takes a list of"
+        " key=value pairs. Where applicable, the default installation"
+        " behavior is to install by downloading and uncompressing"
+        " binaries. Some programs can be built from source.",
+        "afni":
+        "Install AFNI. Valid keys are version (required), method,"
+        " install_path, install_r, install_r_pkgs, install_python2,"
+        " and install_python3. Only the latest version and version"
+        " 17.2.02 are supported at this time.",
+        "ants":
+        "Install ANTs. Valid keys are version (required), method"
+        " install_path, cmake_opts, and make_opts. Version can be a "
+        " git commit hash if building from source.",
+        "convert3d":
+        "Install Convert3D. Valid keys are version (required),"
+        " method, and install_path.",
+        "dcm2niix":
+        "Install dcm2niix. Valid keys are version, method,"
+        " install_path, cmake_opts, and make_opts",
+        "freesurfer":
+        "Install FreeSurfer. Valid keys are version (required),"
+        " method, install_path, and exclude_paths. A FreeSurfer"
+        " license is required to run the software and is not"
+        " provided by Neurodocker.",
+        "fsl":
+        "Install FSL. Valid keys are version (required), method,"
+        " install_path, and exclude_paths.",
+        "matlabmcr":
+        "Install Matlab Compiler Runtime. Valid keys are version,"
+        " method, and install_path",
+        "miniconda":
+        "Install Miniconda. Valid keys are install_path,"
+        " env_name, conda_install, pip_install, conda_opts,"
+        " pip_opts, activate (default false), and version"
+        " (defaults to latest). The options conda_install and"
+        " pip_install accept strings of packages: conda_install="
+        '"python=3.6 numpy traits".',
+        "mrtrix3":
+        "Install MRtrix3. Valid keys are version (required),"
+        " method, and install_path",
+        "ndfreeze":
+        "Use the NeuroDebian command `nd_freeze` to freeze the apt"
+        " sources to a certain date.",
+        "neurodebian":
+        "Add NeuroDebian repository. Valid keys are "
+        "os_codename (e.g., zesty), server (e.g., usa-nh), and"
+        " full (if true, use non-free packages). Valid download"
+        " servers are {}.".format(_ndeb_servers),
+        "spm12":
+        "Install SPM12 and its dependency, Matlab Compiler Runtime."
+        " Valid keys are version and install_path.",
+        "minc":
+        "Install MINC. Valid keys is version (required), method, and"
+        " install_path. Only version 1.9.15 is supported at this"
+        " time.",
+        "petpvc":
+        "Install PETPVC. Valid keys are version (required), method,"
+        " and install_path.",
+        "vnc":
+        "Install a VNC server. Valid keys are passwd (required),"
+        " start_at_runtime, and geometry."
     }
 
     pkgs = p.add_argument_group(
@@ -156,8 +195,13 @@ def _add_generate_common_arguments(parser):
         # MRtrix3 does not need any arguments by default.
         nargs = "*" if pkg == "mrtrix3" else "+"
         pkgs.add_argument(
-            flag, dest=pkg, nargs=nargs, action=OrderedArgs, metavar="",
-            type=_list_of_kv, help=pkgs_help[pkg])
+            flag,
+            dest=pkg,
+            nargs=nargs,
+            action=OrderedArgs,
+            metavar="",
+            type=_list_of_kv,
+            help=pkgs_help[pkg])
 
 
 def _add_generate_docker_arguments(parser):
@@ -166,22 +210,36 @@ def _add_generate_docker_arguments(parser):
 
     # Arguments that should be ordered.
     p.add_argument(
-        '--add', action=OrderedArgs, nargs="+",
+        '--add',
+        action=OrderedArgs,
+        nargs="+",
         help="Dockerfile ADD instruction. Use format <src>... <dest>")
     p.add_argument(
-        '--arg', action=OrderedArgs, nargs="+", type=_list_of_kv,
+        '--arg',
+        action=OrderedArgs,
+        nargs="+",
+        type=_list_of_kv,
         help="Dockerfile ARG instruction. Use format KEY[=DEFAULT_VALUE] ...")
     p.add_argument(
-        '--cmd', action=OrderedArgs, nargs="+",
+        '--cmd',
+        action=OrderedArgs,
+        nargs="+",
         help="Dockerfile CMD instruction.")
     p.add_argument(
-        '--expose', nargs="+", action=OrderedArgs,
+        '--expose',
+        nargs="+",
+        action=OrderedArgs,
         help="Dockerfile EXPOSE instruction.")
     p.add_argument(
-        '--label', action=OrderedArgs, nargs="+", type=_list_of_kv,
+        '--label',
+        action=OrderedArgs,
+        nargs="+",
+        type=_list_of_kv,
         help="Dockerfile LABEL instruction.")
     p.add_argument(
-        '--volume', action=OrderedArgs, nargs="+",
+        '--volume',
+        action=OrderedArgs,
+        nargs="+",
         help="Dockerfile VOLUME instruction.")
 
 
@@ -193,12 +251,16 @@ def _add_generate_singularity_arguments(parser):
 def _add_reprozip_trace_arguments(parser):
     """Add arguments to `parser` for sub-command `reprozip-trace`."""
     p = parser
-    p.add_argument('container',
-                   help="Running container in which to trace commands.")
+    p.add_argument(
+        'container', help="Running container in which to trace commands.")
     p.add_argument('commands', nargs='+', help="Command(s) to trace.")
-    p.add_argument('--dir', '-d', dest="packfile_save_dir", default=".",
-                   help=("Directory in which to save pack file. Default "
-                         "is current directory."))
+    p.add_argument(
+        '--dir',
+        '-d',
+        dest="packfile_save_dir",
+        default=".",
+        help=("Directory in which to save pack file. Default "
+              "is current directory."))
 
 
 def _add_reprozip_merge_arguments(parser):
@@ -216,18 +278,23 @@ def create_parser():
 
     verbosity_choices = ('debug', 'info', 'warning', 'error', 'critical')
     parser.add_argument("-v", "--verbosity", choices=verbosity_choices)
-    parser.add_argument("-V", "--version", action="version",
-                        version=('neurodocker version {}'.format(__version__)))
+    parser.add_argument(
+        "-V",
+        "--version",
+        action="version",
+        version=('neurodocker version {}'.format(__version__)))
 
     subparsers = parser.add_subparsers(
-        dest="subparser_name", title="subcommands",
+        dest="subparser_name",
+        title="subcommands",
         description="valid subcommands")
 
     # `neurodocker generate` parsers.
     generate_parser = subparsers.add_parser(
         'generate', help="generate recipes")
     generate_subparsers = generate_parser.add_subparsers(
-        dest="subsubparser_name", title="subcommands",
+        dest="subsubparser_name",
+        title="subcommands",
         description="valid subcommands")
     generate_docker_parser = generate_subparsers.add_parser(
         'docker', help="generate Dockerfile")
@@ -241,7 +308,8 @@ def create_parser():
     # `neurodocker reprozip` parsers.
     reprozip_parser = subparsers.add_parser('reprozip', help="")
     reprozip_subparsers = reprozip_parser.add_subparsers(
-        dest="subsubparser_name", title="subcommands",
+        dest="subsubparser_name",
+        title="subcommands",
         description="valid subcommands")
     reprozip_trace_parser = reprozip_subparsers.add_parser(
         'trace', help="minify container for traced command(s)")
@@ -318,8 +386,8 @@ def reprozip_merge(namespace):
 
 
 def _validate_generate_args(namespace):
-    if (namespace.file is None and
-       (namespace.base is None or namespace.pkg_manager is None)):
+    if (namespace.file is None
+            and (namespace.base is None or namespace.pkg_manager is None)):
         raise ValueError("-b/--base and -p/--pkg-manager are required if not"
                          " generating from JSON file.")
 
