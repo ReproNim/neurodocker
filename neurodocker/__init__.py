4e2a2be373b1b832de7aadbe8bf9565d7b6c6da8
Jakub Kaczmarzyk <jakub.kaczmarzyk@gmail.com>
2020-07-21 10:33:04 -0400

    add __version__ before other imports

--- neurodocker/__init__.py
+++ neurodocker/__init__.py
@@ -1,12 +1,14 @@
 import sys
 
 from neurodocker._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
+
 from neurodocker.generators import Dockerfile
 from neurodocker.generators import SingularityRecipe
 from neurodocker.utils import set_log_level
 
-__version__ = get_versions()["version"]
-del get_versions
 
 if sys.version_info[0] < 3:
     raise RuntimeError(
